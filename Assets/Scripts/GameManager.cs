c0f006a88aa02c8c87f7149d68b568855c2666d7
Karen Fernandez <121123435+kikitaaa@users.noreply.github.com>
2023-02-05 16:39:22 +0100

    Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private int score = 0;
+    private float time = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+    }
+    public void AddPunt(int value)
+    {
+        score += value;
+    }
+    public int Getpunt()
+    {
+        return score;
+    }
+    public float GetTime()
+    {
+        return time;
+    }
+
+}
